a096186f37d4748066251aac2dd8777e46c35eef
Frederico Guilherme Kluser de Oliveira <kluserhuu@gmail.com>
2022-02-04 19:11:29 -0300

    fix design and position to new item style

--- src/components/organism/ItemModal/ItemModal.tsx
+++ src/components/organism/ItemModal/ItemModal.tsx
@@ -1,23 +1,32 @@
 import React, {useState, useEffect} from 'react';
 import {Animated} from 'react-native';
 import kluserMotion from '../../../helpers/animation';
-import {deviceHeight, deviceWidth} from '../../../helpers/dimentions';
+import {deviceHeight, deviceWidth, itemSize} from '../../../helpers/dimentions';
 import {PAGE_PADDING} from '../../../styles/theme';
 import Title from '../../atom/Title/Title';
 import Container, {viewStyle} from './ItemModal.styled';
 
 const ItemModal = ({index, name, setSelectedItem, scrollY}: any) => {
-  const topSize = 100 * index + 16 * (index + 1);
+  const distanceFromTop = Math.ceil((index + 1) / 2);
+  const sizeMultiplier = distanceFromTop - 1;
+  const marginMultiplier = sizeMultiplier
+    ? PAGE_PADDING * 2 * sizeMultiplier
+    : 0;
+  const topSize = itemSize * sizeMultiplier + marginMultiplier + PAGE_PADDING;
+
+  const leftSize = index % 2 === 0 ? 16 : deviceWidth - (itemSize + 16);
   const animationTime = 500;
 
   const [style, setStyle] = useState({
+    borderRadius: kluserMotion(16, 0),
     top: kluserMotion(topSize, scrollY),
-    left: kluserMotion(16, 0),
-    width: kluserMotion(deviceWidth - PAGE_PADDING * 2, deviceWidth),
-    height: kluserMotion(100, deviceHeight),
+    left: kluserMotion(leftSize, 0),
+    width: kluserMotion(itemSize, deviceWidth),
+    height: kluserMotion(itemSize, deviceHeight),
   });
 
   useEffect(() => {
+    style.borderRadius.start();
     style.top.start();
     style.left.start();
     style.width.start();
@@ -31,11 +40,13 @@ const ItemModal = ({index, name, setSelectedItem, scrollY}: any) => {
         style.top.animation,
         style.width.animation,
         style.height.animation,
+        style.borderRadius.animation,
       )}>
       <Container
         index={index}
         onPress={() => {
           setStyle({
+            borderRadius: style.borderRadius.reverse(),
             top: style.top.reverse(),
             left: style.left.reverse(),
             width: style.width.reverse(),
--- src/helpers/dimentions.ts
+++ src/helpers/dimentions.ts
@@ -1,5 +1,8 @@
 import {Dimensions} from 'react-native';
+import {PAGE_PADDING} from '../styles/theme';
 
 export const deviceWidth = Dimensions.get('window').width;
 
 export const deviceHeight = Dimensions.get('window').height;
+
+export const itemSize = deviceWidth / 2 - PAGE_PADDING * 2;
